bf1f54b63b354e0441ddf4c0484b3103e83c5054
Uwe Tews <uwe.tews@googlemail.com>
2014-04-19 16:56:54 +0200

    Update PHPunit tests

--- development/PHPunit/_runDebugTest.php
+++ development/PHPunit/_runDebugTest.php
@@ -5,10 +5,11 @@
  * @package PHPunit
  * @author Uwe Tews
  */
-include 'smartytestdebug.inc.php';
+include '_runTest.inc.php';
+include '_runDebugTest.inc.php';
 
 // place request PHPunit test class here or leave empty for all
-$_classes = 'CompileNocacheTests';
+$_classes = 'AssignGlobalTests';
 
 // place method name for a singe test here or leave empty for all
 $function = array();
@@ -20,14 +21,14 @@ SmartyTests::$smarty->clearCompiledTemplate();
 // if no classes have been selected scan PHPunit folder for all
 if (empty($_classes)) {
     foreach (new DirectoryIterator(dirname(__FILE__)) as $file) {
-        if (!$file->isDot() && !$file->isDir() && !in_array((string) $file, array('smartytests.php', 'smartytestssingle.php', 'smartytestsfile.php', 'smartytestdebug.php', 'smartytestdebug.inc.php')) && substr((string) $file, -4) === '.php') {
+        if (!$file->isDot() && !$file->isDir() && !in_array((string)$file, array('smartytests.php', 'smartytestssingle.php', 'smartytestsfile.php', 'smartytestdebug.php', 'smartytestdebug.inc.php')) && substr((string)$file, -4) === '.php') {
             $class = basename($file, '.php');
-                 require_once $file->getPathname();
-                // to have an optional test suite, it should implement a public static function isRunnable
-                // that returns true only if all the conditions are met to run it successfully, for example
-                // it can check that an external library is present
-                if (!method_exists($class, 'isRunnable') || call_user_func(array($class, 'isRunnable'))) {
-                    $_classes[] = $class;
+            require_once $file->getPathname();
+            // to have an optional test suite, it should implement a public static function isRunnable
+            // that returns true only if all the conditions are met to run it successfully, for example
+            // it can check that an external library is present
+            if (!method_exists($class, 'isRunnable') || call_user_func(array($class, 'isRunnable'))) {
+                $_classes[] = $class;
             }
         }
     }
@@ -38,7 +39,7 @@ if (empty($_classes)) {
 }
 
 // for all selected test classes
-foreach ((array) $_classes as $class) {
+foreach ((array)$_classes as $class) {
     if (empty($function)) {
         $c = new ReflectionClass('PHPUnit_Framework_TestCase');
         $m1 = $c->getMethods();
@@ -77,23 +78,29 @@ foreach ((array) $_classes as $class) {
 
     echo '<br><br>================   END FIRST PASS  ============<br><br>';
 
-
-    // repeat tests which did fail
-    if (!empty($o->error_functions)) {
     SmartyTests::$smarty->caching_type = 'file';
-//    SmartyTests::$smarty->compiled_type = 'file';
     SmartyTests::$smarty->default_resource_type = 'file';
     SmartyTests::$smarty->clearAllCache();
     SmartyTests::$smarty->clearCompiledTemplate();
+
+    // repeat tests which did fail
+    if (!empty($o->error_functions)) {
         $error_functions = $o->error_functions;
         $o->error_functions = array();
 
         foreach ($error_functions as $func) {
+            try {
                 $o->current_function = $func;
                 $o->setUP();
-                // You may set a debugger breakpoint below for debugging failing tests
                 $o->$func();
-         }
+            } catch (Exception $e) {
+                while (ob_get_level() > 0) {
+                    ob_end_clean();
+                }
+                $o->error();
+                echo '<br>Exception: ', $e->getMessage(), "<br>";
+            }
+        }
     }
     $function = array();
 
