8a889e0eb0c9060d7e92800cbbe035bb68ca25e1
Uwe Tews <uwe.tews@googlemail.com>
2014-06-06 04:53:03 +0200

    - fixed spelling, PHPDoc , format, minor errors, code cleanup

--- demo/plugins/cacheresource.pdo_gzip.php
+++ demo/plugins/cacheresource.pdo_gzip.php
@@ -1,19 +1,20 @@
 <?php
 
-/** 
- * PDO Cache Handler with GZIP support 
- * 
- * Example usage : 
- *      $cnx    =   new PDO("mysql:host=localhost;dbname=mydb", "username", "password"); 
- *      $smarty->setCachingType('pdo_gzip'); 
- *      $smarty->loadPlugin('Smarty_CacheResource_Pdo_Gzip'); 
- *      $smarty->registerCacheResource('pdo_gzip', new Smarty_CacheResource_Pdo_Gzip($cnx, 'smarty_cache')); 
- * 
- * @require Smarty_CacheResource_Pdo class 
- * @author Beno!t POLASZEK - 2014 
- */ 
+/**
+ * PDO Cache Handler with GZIP support
+ * Example usage :
+ *      $cnx    =   new PDO("mysql:host=localhost;dbname=mydb", "username", "password");
+ *      $smarty->setCachingType('pdo_gzip');
+ *      $smarty->loadPlugin('Smarty_CacheResource_Pdo_Gzip');
+ *      $smarty->registerCacheResource('pdo_gzip', new Smarty_CacheResource_Pdo_Gzip($cnx, 'smarty_cache'));
+ *
+ * @require Smarty_CacheResource_Pdo class
+ * @author  Beno!t POLASZEK - 2014
+ */
 require_once 'cacheresource.pdo.php';
-class Smarty_CacheResource_Pdo_Gzip extends Smarty_CacheResource_Pdo { 
+
+class Smarty_CacheResource_Pdo_Gzip extends Smarty_CacheResource_Pdo
+{
 
     /* 
      * Encodes the content before saving to database 
@@ -21,10 +22,11 @@ class Smarty_CacheResource_Pdo_Gzip extends Smarty_CacheResource_Pdo {
      * @param string $content 
      * @return string $content 
      * @access protected 
-     */ 
-    protected function inputContent($content) { 
-        return gzdeflate($content); 
-    } 
+     */
+    protected function inputContent($content)
+    {
+        return gzdeflate($content);
+    }
 
     /* 
      * Decodes the content before saving to database 
@@ -32,10 +34,10 @@ class Smarty_CacheResource_Pdo_Gzip extends Smarty_CacheResource_Pdo {
      * @param string $content 
      * @return string $content 
      * @access protected 
-     */ 
-    protected function outputContent($content) { 
-        return gzinflate($content); 
-    } 
-
+     */
+    protected function outputContent($content)
+    {
+        return gzinflate($content);
+    }
 } 
  
